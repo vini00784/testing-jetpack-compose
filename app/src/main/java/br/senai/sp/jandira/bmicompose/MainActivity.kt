fb42b957e3c3200a8970cef38ad860042e629d96
vini00784 <vo70346@gmail.com>
2023-02-06 09:58:25 -0300

    doing animations

--- app/src/main/java/br/senai/sp/jandira/bmicompose/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicompose/MainActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmicompose.ui.theme.BMIComposeTheme
@@ -42,6 +46,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun BMICalculator() {
 
@@ -93,11 +98,10 @@ fun BMICalculator() {
                 fontSize = 16.sp
             )
             OutlinedTextField(
-                value = weightState, onValueChange = {
-                    var lastChar = if (it.length == 0)
-                        it
-                    else
-                        it[it.length - 1]
+                value = weightState,
+                onValueChange = {
+                    var lastChar = if (it.length == 0) it
+                    else it[it.length - 1]
 
                     var newValue = if (lastChar == '.' || lastChar == ',') it.dropLast(1) else it
 
@@ -117,11 +121,10 @@ fun BMICalculator() {
                 fontSize = 16.sp
             )
             OutlinedTextField(
-                value = heightState, onValueChange = {
-                    var lastChar = if (it.length == 0)
-                        it
-                    else
-                        it[it.length - 1]
+                value = heightState,
+                onValueChange = {
+                    var lastChar = if (it.length == 0) it
+                    else it[it.length - 1]
 
                     var newValue = if (lastChar == '.' || lastChar == ',') it.dropLast(1) else it
 
@@ -133,7 +136,9 @@ fun BMICalculator() {
             )
 
             Button(
-                onClick = {},
+                onClick = {
+                    footerExpandState = true
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 32.dp)
@@ -152,65 +157,80 @@ fun BMICalculator() {
         }
 
         // Footer
-        Card(
-            modifier = Modifier
-                .fillMaxHeight(0.7f)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(topEnd = 32.dp, topStart = 32.dp),
-            backgroundColor = MaterialTheme.colors.primary
+        AnimatedVisibility(
+            visible = footerExpandState,
+            enter = expandVertically(expandFrom = Alignment.Bottom) {
+                20
+            },
+            exit = shrinkVertically(shrinkTowards = Alignment.Bottom)
+//            enter = slideIn(tween(durationMillis = 500)) {
+//                IntOffset(it.width, 100)
+//            },
+//            exit = slideOut(tween(durationMillis = 500)) {
+//                IntOffset(it.width, 100)
+//            }
         ) {
-            Column(
+
+            Card(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxHeight(0.7f)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(topEnd = 32.dp, topStart = 32.dp),
+                backgroundColor = MaterialTheme.colors.primary
             ) {
-                Text(
-                    text = stringResource(id = R.string.your_score),
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.your_score),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
 
-                Text(
-                    text = "0.00",
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                    Text(
+                        text = "0.00", fontSize = 40.sp, fontWeight = FontWeight.Bold
+                    )
 
-                Text(
-                    text = "Congratulations! Your weight is ideal!",
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
+                    Text(
+                        text = "Congratulations! Your weight is ideal!",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
 
-                Row() {
+                    Row() {
 
-                    Button(
-                        onClick = { /*TODO*/ },
-                        colors = ButtonDefaults.buttonColors(Color(153, 111, 221, 255))
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.reset_button),
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.W600
-                        )
+                        Button(
+                            onClick = {
+                                footerExpandState = false
+                            },
+                            colors = ButtonDefaults.buttonColors(Color(153, 111, 221, 255))
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.reset_button),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.W600
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Button(
+                            onClick = { /*TODO*/ },
+                            colors = ButtonDefaults.buttonColors(Color(153, 111, 221, 255))
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.share_button),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.W600
+                            )
+                        }
+
                     }
-
-                    Spacer(modifier = Modifier.width(16.dp))
-
-                    Button(
-                        onClick = { /*TODO*/ },
-                        colors = ButtonDefaults.buttonColors(Color(153, 111, 221, 255))
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.share_button),
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.W600
-                        )
-                    }
-
                 }
             }
         }
